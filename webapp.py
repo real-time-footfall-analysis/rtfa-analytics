4f0da4196ec5c42f0a991d0cb425654159244580
Rohan Pritchard <rohanpritchard@icloud.com>
2018-10-31 23:17:51 +0000

    Run app on 0.0.0.0.

--- webapp.py
+++ webapp.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/health")
 def health():
-    return 200
+    return '', 200
 
 
 @app.route("/compute/<int:freqGroupID>")
@@ -17,4 +17,4 @@ def handle_compute(freqGroupID):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=80)
+    app.run(debug=True, host='0.0.0.0')
